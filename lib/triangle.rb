cb661d2866db98ff70f61fea6a1e25869a9da0eb
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-04-28 20:11:34 -0400

    Updated Triangle Class

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+  	@a = a
+  	@b = b
+  	@c = c
+  end
+
+  def kind
+  	is_triangle?
+  	if a == b && b == c
+  		:equilateral
+	elsif a == b || b == c || a == c
+		:isosceles
+	else
+		:scalene
+	end
+  end
+
+  def is_triangle?
+  	triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  
+  	[a, b, c].each {|side| triangle << false if side <= 0 }
+
+  	raise TriangleError if triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
